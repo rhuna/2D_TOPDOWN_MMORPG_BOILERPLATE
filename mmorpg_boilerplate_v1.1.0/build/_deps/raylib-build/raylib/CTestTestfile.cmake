436365adcc1c184416794abffdc132026f0ba2bb
Brandon Autry <bsautry@ualr.edu>
2026-04-02 20:07:23 -0500

    added chat

--- mmorpg_boilerplate_v1.1.0/build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/MMORPG_boilerplate/mmorpg_repo/build/_deps/raylib-src/src
-# Build directory: C:/dev/MMORPG_boilerplate/mmorpg_repo/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
